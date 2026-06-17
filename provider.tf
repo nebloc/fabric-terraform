17c969763b8f88edbdba3e924d15fc9bdac9beb1
Ben Coleman <becoleman@microsoft.com>
2026-06-17 14:13:29 +0100

    Attempt 1

--- provider.tf
+++ provider.tf
@@ -14,15 +14,13 @@ terraform {
       version = "~> 3.1.0"
     }
   }
+  # State is stored in an Azure Storage account and authenticated with the
+  # service principal supplied by the Azure DevOps pipeline. Authentication
+  # (tenant_id / client_id / client_secret / subscription_id) is taken from the
+  # ARM_* environment variables exported in the pipeline, and the storage
+  # account / container / key are passed at init time via -backend-config.
   backend "azurerm" {
-    use_oidc                         = true                                    # Can also be set via `ARM_USE_OIDC` environment variable.
-    oidc_azure_service_connection_id = "00000000-0000-0000-0000-000000000000"  # Can also be set via `ARM_OIDC_AZURE_SERVICE_CONNECTION_ID` environment variable.
-    use_azuread_auth                 = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
-    tenant_id                        = "00000000-0000-0000-0000-000000000000"  # Can also be set via `ARM_TENANT_ID` environment variable.
-    client_id                        = "00000000-0000-0000-0000-000000000000"  # Can also be set via `ARM_CLIENT_ID` environment variable.
-    storage_account_name             = "abcd1234"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name                   = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                              = "prod.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    use_azuread_auth = true # Authenticate to the storage account as the SPN via Entra ID (`ARM_USE_AZUREAD`).
   }
 }
 
